88b17b3e15b8aa85df1274fa47f11356816f354e
miguel <miguel131299@gmail.com>
2021-03-16 19:52:12 +0100

    Implement: Display Numbers

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+//select elements
+const numberButtons = document.querySelectorAll(".number-button");
+const operationButtons = document.querySelectorAll(".operation-button");
+const displayOperation = document.getElementById("display-operation");
+const displayResult = document.getElementById("display-result");
+const acButton = document.getElementById("AC-button");
+const delButton = document.getElementById("delete-button");
+const ansButton = document.getElementById("answer=button");
+const resultButton = document.getElementById("result-button");
+
+//add event listener to numbers
+numberButtons.forEach(button => button.addEventListener("click", appendToDisplayOperation));
+
+
+function appendToDisplayOperation(e) {
+    if (displayOperation.textContent.length < 26) {
+
+        if (displayOperation.textContent === "0") {
+            displayOperation.textContent = this.textContent;
+        } else {
+            displayOperation.textContent += this.textContent;
+        }
+    
+    } else {
+        //TODO: print error message
+    }
+}
+
+
+//Operation functions
+
 const add = (a, b) => a + b;
 
 const substract = (a, b) => a - b;
@@ -5,6 +36,8 @@ const substract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 
 const divide = (a, b) => a / b;
+
+const modulo = (a, b) => a % b;
 
 const operate = function (operator, a, b) {
     
@@ -26,6 +59,14 @@ const operate = function (operator, a, b) {
             }
 
             return divide(a, b);
+
+        case "mod":
+            //avoid division by 0
+            if (b === 0) {
+                return "MATH ERROR";
+            }
+
+            return modulo(a, b);
     
         default:
             break;
